559e2e7ac64b737af6389da1c0289d9099b5fbdd
sakshi9401 <68016527+sakshi9401@users.noreply.github.com>
2020-07-21 14:47:38 +0530

    Add files via upload

---
+++ forloop2.py
@@ -0,0 +1,25 @@
+# looping through string
+for x in "banana":
+    print(x)
+
+#break statement to print upto c++
+s = ["c","java","c++","python"]
+for x in s:
+    print(x)
+    if x=="c++":
+        break
+
+#break statement to print all elements before banana
+s = ["apple","banana","cherry"]
+for x in s:
+    if x=="banana" :
+        break
+    print(x)
+
+#range
+for x in range(10):
+    print(x)
+
+#range
+for x in range(1,11):
+    print(x)
