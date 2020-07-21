9adf6c87c22dab90cc4cdd06d75b98122848eaff
sakshi9401 <68016527+sakshi9401@users.noreply.github.com>
2020-07-21 14:21:08 +0530

    Add files via upload

---
+++ forloop1.py
@@ -0,0 +1,25 @@
+#list
+
+l = [1,2,3,4,5,6,7,8,9,10]
+for x in l:
+    print(x)
+
+#set
+
+s = {45,67,87,23,45,78,97,32,22}
+for x in s:
+    print(x)
+
+#tuple
+
+t = ("c","c++","java","python","html","css")
+for x in t:
+    print(x)
+
+#dictionary
+
+d = {"operator":"addition",
+     "book":"newtons",
+     "problem":1}
+for x in d:
+    print(x)
