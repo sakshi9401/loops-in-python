2e44705afa5d466caa99418e39a1876e4bab172e
sakshi9401 <68016527+sakshi9401@users.noreply.github.com>
2020-07-22 13:42:45 +0530

    Add files via upload

---
+++ while1.py
@@ -0,0 +1,19 @@
+i=1
+while i<=10:
+    print(i)
+    i = i+1
+
+#break statement
+i=1
+while i<10:
+    print(i)
+    if i%2==0:
+        break
+        i=i+1
+
+#table of 2 through break statement
+i=1
+while i<=20:
+    i= i+1
+    if i%2==0:
+        print(i)
